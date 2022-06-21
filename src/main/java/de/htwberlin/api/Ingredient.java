10b3e6424d28ff36e192f1731a6092ec5d710b3e
Hoa <DucHoa.Le@Student.HTW-Berlin.de>
2022-06-21 15:49:12 +0200

    fixng local class

--- src/main/java/de/htwberlin/api/Ingredient.java
+++ src/main/java/de/htwberlin/api/Ingredient.java
@@ -1,5 +1,7 @@
 package de.htwberlin.api;
 
+import java.util.List;
+
 public class Ingredient {
 
     private long id;
@@ -9,6 +11,7 @@ public class Ingredient {
     private String image;
     private boolean vegetarian;
     private boolean vegan;
+    private List<Long> ingredients;
 
     public Ingredient(long id, String name, boolean vegetarian, boolean vegan) {
         this.id = id;
@@ -83,44 +86,5 @@ public class Ingredient {
                 '}';
     }
 
-//    private long id;
-//    private String ingName;
-//    private boolean vegetarian;
-//    private boolean vegan;
-//
-//    public Ingredient(long id, String ingName, boolean vegetarian, boolean vegan) {
-//        this.id = id;
-//        this.ingName = ingName;
-//        this.vegan = vegan;
-//        this.vegetarian = vegetarian;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public String getIngName() {
-//        return ingName;
-//    }
-//
-//    public void setIngName(String ingName) {
-//        this.ingName = ingName;
-//    }
-//
-//    public boolean isVegetarian() {
-//        return vegetarian;
-//    }
-//
-//    public void setVegetarian(boolean vegetarian) {
-//        this.vegetarian = vegetarian;
-//    }
-//
-//    public boolean isVegan() {
-//        return vegan;
-//    }
-//
-//    public void setVegan(boolean vegan) {
-//        this.vegan = vegan;
-//    }
 }
 
--- src/main/java/de/htwberlin/api/Recipe.java
+++ src/main/java/de/htwberlin/api/Recipe.java
@@ -160,37 +160,4 @@ public class Recipe {
                 '}';
     }
 
-//    private List<Ingredient> ingredients;
-//    private String recipeName;
-//    private long id;
-//
-//    public Recipe(long id, String recipeName, ArrayList<Ingredient> ing) {
-//        this.id = id;
-//        this.recipeName = recipeName;
-//        this.ingredients = ing;
-//    }
-//
-//    public List<Ingredient> getIngredients() {
-//        return ingredients;
-//    }
-//
-//    public void setIngredients(List<Ingredient> ingredients) {
-//        this.ingredients = ingredients;
-//    }
-//
-//    public String getRecipeName() {
-//        return recipeName;
-//    }
-//
-//    public void setRecipeName(String recipeName) {
-//        this.recipeName = recipeName;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
 }
